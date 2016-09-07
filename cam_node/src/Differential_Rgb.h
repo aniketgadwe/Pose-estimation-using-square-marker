b16a0edd573286fe96ad4a610c64a20165b37fa8
aniketgadwe <gadwe.aniket@rediffmail.com>
2016-09-07 16:03:37 +0530

    Update Differential_Rgb.h

--- cam_node/src/Differential_Rgb.h
+++ cam_node/src/Differential_Rgb.h
@@ -8,16 +8,7 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
 #include "opencv2/core/core.hpp"
-#include <cv.h>
 #include <iostream>
-#include <stdio.h>
-#include <math.h>
-#include <cmath>
-#include <stdlib.h>
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
 
 using namespace std;
 using namespace cv;
